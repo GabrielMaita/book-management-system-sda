b476c97c591dc22d72007c10cf8c070736bc6e90
DESKTOP-PB2732I\gmaita00 <gmaita00@gmail.com>
2022-11-16 19:08:50 +0100

    Add generic repositories

---
+++ src/main/java/com/sda/gabrielm/bookmanagement/repository/BaseRepository.java
@@ -0,0 +1,17 @@
+package com.sda.gabrielm.bookmanagement.repository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface BaseRepository<T> {
+    Optional<T> findById(Integer id);
+
+    void create(T entity);
+
+    void update(T entity);
+
+    void delete(T entity);
+
+}
